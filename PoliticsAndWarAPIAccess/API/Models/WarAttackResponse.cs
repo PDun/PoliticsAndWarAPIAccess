e4032a94a4fde0fdb8d7dac385bb4d1e0c41525f
Paul Muir <pmuir@paylocity.com>
2018-11-25 12:28:47 -0600

    Adding in the War Attack API

---
+++ PoliticsAndWarAPIAccess/API/Models/WarAttackResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoliticsAndWarAPIAccess.API.Models
+{
+  public class WarAttackResponse
+  {
+    public bool success { get; set; }
+    public List<WarAttack> war_attacks { get; set; }
+  }
+}
